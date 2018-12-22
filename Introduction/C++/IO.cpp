7d0e4d2f942c1ef87683ea0271179efefd2b0a81
Matt Gleason <gleason2@purdue.edu>
2018-12-22 01:39:45 -0500

    Added C++ examples

---
+++ Introduction/C++/IO.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	//C++ uses the << operator to print to stdout.
+	//You can easily take input from stdin using the >> operator.
+	char c;
+	int d;
+	float f;
+	string s;
+	cout << "Type a character, decimal, float, and string. I will add commas.\n";
+	cin >> c >> d >> f >> s;
+	cout << c << ", " << d << ", " << f << ", " << s << '\n';
+
+}
