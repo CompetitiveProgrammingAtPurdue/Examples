d56aa4b565ff25d5f8d3657bc9dcdd24ea8f6096
Matt Gleason <gleason2@purdue.edu>
2019-01-17 16:50:17 -0500

    Added stacks and queues to C++ examples

--- Data_Structures/C++/Structures.cpp
+++ Data_Structures/C++/Structures.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>   //http://www.cplusplus.com/reference/list/list/
 #include <vector> //http://www.cplusplus.com/reference/vector/vector/
+#include <stack>  //http://www.cplusplus.com/reference/stack/stack/
 #include <queue>  //http://www.cplusplus.com/reference/queue/priority_queue/
 
 using namespace std;
@@ -22,53 +23,80 @@ struct Compare {
 };
 
 int main() {
+  cout << "Lists:\n";
   //Create a list by specifying a type.
-  list<int> a;
+  list<int> l;
   //Push an item to the back or front of the list.
-  a.push_back(2);
-  a.push_front(0);
+  l.push_back(2);
+  l.push_front(0);
   //Insert items. This requires an iterator. 
-  list<int>::iterator i = a.begin();
+  list<int>::iterator i = l.begin();
   //For lists, iterators can only support ++ and -- operations.
   i++;
-  a.insert(i, 1);
+  l.insert(i, 1);
   //Access items.
   //In C++, you need an iterator to access elements in a list.
-  for (i = a.begin(); i != a.end(); i++) {
+  for (i = l.begin(); i != l.end(); i++) {
     cout << *i << ' ';
   }
   cout << '\n';
   //You can also get the size easily.
-  cout << a.size() << '\n';
+  cout << l.size() << "\n\n";
 
+  cout << "Vectors:\n";
   //Create a vector by specifying a type.
-  vector<int> b({0, 1, 2});
+  vector<int> v({0, 1, 2});
   //Push and insert items like lists, access with [] operation.
   //This allows random access.
   int j;
-  for (j = 0; j < b.size(); j++) {
-    cout << b[j] << ' ';
-    b[j] = b.size() - j;
+  for (j = 0; j < v.size(); j++) {
+    cout << v[j] << ' ';
+    v[j] = v.size() - j;
   }
   cout << '\n';
-  for (j = 0; j < b.size(); j++) {
-    cout << b[j] << ' ';
+  for (j = 0; j < v.size(); j++) {
+    cout << v[j] << ' ';
   }
-  cout << '\n';
-  
+  cout << "\n\n";
+
+  cout << "Stacks:\n";
+  //Create a stack with a type.
+  stack<int> s;
+  //Push, pop, top, empty, and size are the basic functions.
+  //Push and pop operate on the top of the stack.
+  s.push(0);
+  s.push(1);
+  cout << s.top() << '\n';
+  s.pop();
+  cout << s.top() << '\n';
+  cout << s.size() << "\n\n";
+
+  cout << "Queues:\n";
+  //Create a queue with a type.
+  queue<int> q;
+  //Push, pop, front, back, size, and empty are the basic functions.
+  //Push pushes to the back of the queue and pop pops from the front.
+  q.push(0);
+  q.push(1);
+  cout << q.front() << '\n';
+  q.pop();
+  cout << q.front() << '\n';
+  cout << q.size() << "\n\n";
+
+  cout << "Priority Queues:\n";
   //Create a priority queue. C++ supports custom comparison operators.
   //It is a max queue by default, so changing your comparator to "greater" will make it  a min queue.
-  priority_queue<block, vector<block>, Compare> c;
+  priority_queue<block, vector<block>, Compare> pq;
   //Push or emplace objects.
-  block d(0, 0);
-  c.push(d);
-  c.emplace(2, 2);
-  c.emplace(1, 1);
-  c.emplace(3, 3);
+  block b(0, 0);
+  pq.push(b);
+  pq.emplace(2, 2);
+  pq.emplace(1, 1);
+  pq.emplace(3, 3);
   //Use top to peek at the top element.
-  cout << c.top().value << '\n';
+  cout << pq.top().value << '\n';
   //Use pop to remove the top element.
-  c.pop();
+  pq.pop();
   //Look again to see the next highest.
-  cout << c.top().value << '\n';
+  cout << pq.top().value << '\n';
 }
